5de890bc817ad0799fa29fc4c43c43e3a10e1f93
Alice-Retz <alicegretz@gmail.com>
2021-11-03 18:16:54 -0500

    Search.jsx and ArticleList.jsx both written

---
+++ components/articles/ArticleList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Article from './Article';
+
+export default function ArticleList({ articles }) {
+  const articleList = articles.map(({ title, author, description }) => (
+    <li key={`${title}`}>
+      <Article title={title} author={author} description={description} />
+    </li>
+  ));
+  return articleList;
+}
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      article: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ),
+};
---
+++ components/articles/Search.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function Search({ searchParam, onSearchChange, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="searchParam">New Search</label>
+      <input
+        id="searchParam"
+        name="searchParam"
+        type="text"
+        value={searchParam}
+        onChange={onSearchChange}
+      />
+      <button aria-label="search">Search</button>
+    </form>
+  );
+}
+
+Search.propTypes = {
+  searchParam: PropTypes.string.isRequired,
+  onSearchChange: PropTypes.string.isRequired,
+  onSubmit: PropTypes.string.isRequired,
+};
